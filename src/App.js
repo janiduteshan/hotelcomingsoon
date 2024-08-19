018c02e8540feda7ed626774b6177615e6dbf015
Janidu Teshan <41275180+janiduteshan@users.noreply.github.com>
2024-08-19 15:32:34 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React from "react";
+import { SocialIcon } from "react-social-icons";
 
 const MaintenancePage = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
-      <div className="text-center p-6">
-        <h1 className="text-4xl font-bold mb-4">Hotel Buddima Samudra</h1>
-        <p className="text-lg mb-4">
-          We're currently working on our website. Please check back soon!
+    <div className="min-h-screen flex flex-col items-center justify-between bg-black text-white p-4">
+      <div className="flex flex-col items-center justify-center flex-grow">
+        <h1 className="text-5xl font-extrabold mb-6 bg-white text-transparent bg-clip-text text-center">
+          Hotel Buddima Samudra
+        </h1>
+        <p className="text-2xl mb-8 text-center font-light">
+          We're doing something cool on our website. BRB!
         </p>
-        <div className="relative mt-10 mb-10">
-          <div className="animate-spin-slow w-20 h-20 border-4 border-solid border-gray-300 border-t-transparent rounded-full mx-auto"></div>
-        </div>
-        <p className="text-sm">Thank you for your patience.</p>
       </div>
+      <div className="flex justify-center space-x-6 mb-6">
+        <p className="text-xl text-center font-medium">
+          Till then, follow us on socials ;)
+        </p>
+      </div>
+      <div className="flex justify-center space-x-6 mb-6">
+        <SocialIcon
+          url="https://www.instagram.com/hotelbuddimasamudra"
+          bgColor="#000000"
+        />
+        <SocialIcon
+          url="https://www.facebook.com/buddimasamudra"
+          bgColor="#000000"
+        />
+      </div>
+      <p>© 2024 Hotel Buddima Samudra. All rights reserved.</p>
     </div>
   );
 };
